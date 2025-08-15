6132bb82298fb43f2d4a9bc653697bb53d42a57b
amadou sy <amsada93@gmail.com>
2025-08-15 22:33:33 +0200

    wip

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 import logging
-from urllib.parse import urlparse
 
 import mlflow
-import os, tempfile, zipfile
+import os, tempfile
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
@@ -12,8 +11,10 @@ from fastapi import FastAPI
 from routes import router
 from settings import get_settings
 import tensorflow as tf
-import keras
+#import keras
 from utils import MeanIoUArgmax, Model, dice_loss
+#import keras_hub
+#from keras_hub.models import MiTBackbone
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -24,20 +25,66 @@ conf = get_settings()
 MLFLOW_TRACKING_URI = conf.MLFLOW_TRACKING_URI
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
+
+# --- compat_keras_hub_mit.py (ou directement en haut de ton main) ---
+import inspect
+#from keras.saving import register_keras_serializable
+
+# Importe les vraies classes depuis l'API publique installée
+#from keras_hub.src.models.mit.mit_layers import MixFFN as KHMixFFN
+#from keras_hub.src.models.mit.mit_layers import OverlappingPatchingAndEmbedding as OPE
+#from keras_hub.src.models.mit.mit_layers import HierarchicalTransformerEncoder as KHEnc
+
+# Enregistre des alias sous le *chemin sérialisé* de ton .keras
+# register_keras_serializable(package="keras_hub.src.models.mit.mit_layers")(OPE)
+# register_keras_serializable(package="keras_hub.src.models.mit.mit_layers")(KHMixFFN)
+# @register_keras_serializable(package="keras_hub.src.models.mit.mit_layers")
+# class HierarchicalTransformerEncoderCompat(KHEnc):
+#     @classmethod
+#     def from_config(cls, cfg):
+#         cfg = dict(cfg)
+#         cfg.pop('mlp')
+#         # ne touche qu'au strict nécessaire
+#         if "drop_prop" in cfg:
+#             sig = inspect.signature(KHEnc.__init__)
+#             params = sig.parameters
+#             if "drop_rate" in params: cfg["drop_rate"] = cfg.pop("drop_prop")
+#             elif "dropout" in params: cfg["dropout"] = cfg.pop("drop_prop")
+#             elif "dropout_rate" in params: cfg["dropout_rate"] = cfg.pop("drop_prop")
+#             else: cfg.pop("drop_prop", None)
+#         return cls(**cfg)
+
 async def load_model():
+    os.environ.setdefault("MLFLOW_HTTP_REQUEST_TIMEOUT", "6000")
     client = mlflow.MlflowClient()
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
             model_path = client.download_artifacts(
-                conf.RUN_ID,
-                "model",
-                dst_path=temp_dir
-            )
-            keras_model_path = os.path.join(model_path, "data", "model.keras")
-            logger.info(f"Loading model from: {keras_model_path}")
-
-            # Charger le modèle avec Keras 3.x
-            model = tf.keras.models.load_model(keras_model_path, custom_objects={"MeanIoUArgmax": MeanIoUArgmax, "dice_loss": dice_loss})
+                 conf.RUN_ID,
+                 "artifacts",
+                 dst_path=temp_dir
+             )
+            keras_model_path = os.path.join(model_path, "mit_segformer_model.keras")
+            # logger.info(f"Loading model from: {keras_model_path}")
+            #
+            # # Charger le modèle avec Keras 3.x
+            # model = keras.models.load_model(
+            #     keras_model_path,
+            #     compile=False,
+            #     safe_mode=True,
+            #     custom_objects={
+            #         #"MeanIoUArgmax": MeanIoUArgmax,
+            #         "dice_loss": dice_loss,
+            #         "HierarchicalTransformerEncoder": HierarchicalTransformerEncoderCompat,
+            #         "MixFFN": KHMixFFN,
+            #     }
+            # )
+            # Télécharge le dossier "model" (qui contient MLmodel + dossier "data/")
+            #local_model_dir = mlflow.artifacts.download_artifacts(artifact_uri=f"runs:/{conf.RUN_ID}/model")
+            #savedmodel_dir = os.path.join(local_model_dir, "data")  # <- le vrai SavedModel est ici
+            # Deux choix:
+            # 1) obtenir un tf.keras.Model:
+            model = tf.keras.models.load_model(keras_model_path, compile=False)
             logger.info("Model loaded, summary :")
             model.summary()
             Model().set_model(model)
