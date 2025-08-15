987c75d07512f4503f2ea16d30a27b8e67397411
amadou sy <amsada93@gmail.com>
2025-08-15 22:33:33 +0200

    wip

--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 import logging
-from urllib.parse import urlparse
 
 import mlflow
-import os, tempfile, zipfile
+import os, tempfile
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
@@ -12,8 +11,8 @@ from fastapi import FastAPI
 from routes import router
 from settings import get_settings
 import tensorflow as tf
-import keras
-from utils import MeanIoUArgmax, Model, dice_loss
+
+from utils import MeanIoUArgmax, Model
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -24,20 +23,31 @@ conf = get_settings()
 MLFLOW_TRACKING_URI = conf.MLFLOW_TRACKING_URI
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
+from keras_hub.src.models.mit.mit_layers import MixFFN as KHMixFFN
+from keras_hub.src.models.mit.mit_layers import OverlappingPatchingAndEmbedding as OPE
+from keras_hub.src.models.mit.mit_layers import HierarchicalTransformerEncoder as KHEnc
+
 async def load_model():
+    os.environ.setdefault("MLFLOW_HTTP_REQUEST_TIMEOUT", "6000")
     client = mlflow.MlflowClient()
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
             model_path = client.download_artifacts(
-                conf.RUN_ID,
-                "model",
-                dst_path=temp_dir
+                 conf.RUN_ID,
+                 "artifacts",
+                 dst_path=temp_dir
+             )
+            keras_model_path = os.path.join(model_path, "mit_segformer_model.keras")
+            model = tf.keras.models.load_model(
+                keras_model_path,
+                compile=False,
+                custom_objects={
+                    "MeanIoUArgmax": MeanIoUArgmax,
+                    "MixFFN": KHMixFFN,
+                    "OverlappingPatchingAndEmbedding": OPE,
+                    "HierarchicalTransformerEncoder": KHEnc,
+                }
             )
-            keras_model_path = os.path.join(model_path, "data", "model.keras")
-            logger.info(f"Loading model from: {keras_model_path}")
-
-            # Charger le modèle avec Keras 3.x
-            model = tf.keras.models.load_model(keras_model_path, custom_objects={"MeanIoUArgmax": MeanIoUArgmax, "dice_loss": dice_loss})
             logger.info("Model loaded, summary :")
             model.summary()
             Model().set_model(model)
